1d612742841e56c1ad3e1c9e9abef74af5c56da6
Ehsan <ehsan.doroudian@gmail.com>
2024-02-19 20:04:38 +0330

    gererate random password

--- code1.py
+++ code1.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 
+from generate_password import *
+
 window = tk.Tk()
 
 defual_setting_dict = {
@@ -10,6 +12,7 @@ defual_setting_dict = {
     "Space" : False
 }
 
+        
 label_featurs_list = [
     {
         "text": "UpperCase: ",
@@ -56,16 +59,39 @@ row_num = 0
 for lable_objcts in label_created_list:
     lable_objcts.grid(row=row_num, column=0, columnspan=5)
     row_num += 1
+    
+yes_button_commands = [
+    {
+        "command" : lambda: defual_setting_dict.update({"UpperCase": True}),
+    },
+    
+    {
+        "command" : lambda: defual_setting_dict.update({"LowerCase": True}),
+    },
+        
+    {
+        "command" :lambda: defual_setting_dict.update({"Number": True}),
+    },
+            
+    {
+        "command" :lambda: defual_setting_dict.update({"Symbol": True}),
+    },
+                
+    {
+        "command" :lambda: defual_setting_dict.update({"Space": True}),
+    },
+                    
+]
  
 yes_btn_list = []
  
-for i in range(5):  
+for yes_btn in yes_button_commands:  
     yes_btn = tk.Button(
         master = window,
-        text= f'yes {i}',
+        text= 'yes',
         width= 8,
         height= 1,
-        command= quit,
+        command= yes_btn["command"],
     )
     yes_btn_list.append(yes_btn)
 
@@ -73,17 +99,39 @@ row_num = 0
 for yes_btn_objs in yes_btn_list:
     yes_btn_objs.grid(row=row_num, column=5, columnspan=15)
     row_num += 1
+  
+no_button_commands = [
+    {
+        "command" : lambda: defual_setting_dict.update({"UpperCase": False}),
+    },
     
+    {
+        "command" : lambda: defual_setting_dict.update({"LowerCase": False}),
+    },
+        
+    {
+        "command" :lambda: defual_setting_dict.update({"Number": False}),
+    },
+            
+    {
+        "command" :lambda: defual_setting_dict.update({"Symbol": False}),
+    },
+                
+    {
+        "command" :lambda: defual_setting_dict.update({"Space": False}),
+    },
+                    
+]  
 
 no_btn_list = []
  
-for i in range(5):  
+for no_btn in no_button_commands:  
     no_btn = tk.Button(
         master = window,
-        text= f'no {i}',
+        text= 'no',
         width= 8,
         height= 1,
-        command= quit,
+        command= no_btn['command'],
     )
     no_btn_list.append(no_btn)
 
@@ -147,15 +195,24 @@ for label_obj in default_label_featurs:
     
 row_num = 0
 for label_objs in default_label_list:
-    label_objs.grid(row=row_num, column=30, columnspan=10, )
+    label_objs.grid(row=row_num, column=30, columnspan=10,)
     row_num += 1
+    
+def generate_random_password_gui():
+    on_options = []
+    for option in defual_setting_dict:
+        if defual_setting_dict[option] == True:
+            on_options.append(option)
+    length = length_ent.get()
+    result = generate_random_password(on_options, int(length))
+    random_password_lbl['text'] = result
     
 run_btn = tk.Button(
     master=window,
     width=4,
     height=1,
     text="Run",
-    command=quit,
+    command=generate_random_password_gui,
 )
 run_btn.grid(row=row_num, column=27, columnspan=10)
 
@@ -167,4 +224,5 @@ reset = tk.Button(
     command=quit,
 )
 reset.grid(row=row_num, column=32, columnspan=10)
+
 window.mainloop()
---
+++ generate_password.py
@@ -0,0 +1,59 @@
+import random
+from string import ascii_lowercase, ascii_uppercase
+
+def add_lowercase():
+    choice = random.choice(ascii_lowercase)
+    return choice
+
+def add_uppercase():
+    choice = random.choice(ascii_uppercase)
+    return choice
+
+def add_symbol():
+    symbol_1 = random.choice(range(33, 48))
+    symbol_2 = random.choice(range(91,97))
+    symbol_3 = random.choice(range(123, 127))
+    choice = random.choice([symbol_1, symbol_2, symbol_3])
+    return chr(choice)
+
+def add_number():
+    choice = random.choice(range(0,10))
+    return str(choice)
+
+def generate_random_password(true_options : list, length : int = 8):
+    my_password = ""
+    
+    len_password = length
+    for password in range(len_password):
+        random_choice = random.choice(true_options)
+        
+        if random_choice == "LowerCase":
+            password = add_lowercase()
+            my_password += password
+        
+        elif random_choice == "UpperCase":
+            password = add_uppercase()
+            my_password += password
+        
+        elif random_choice == "Symbol":
+            password = add_symbol()
+            my_password += password
+        
+        elif random_choice == "Number":
+            password = add_number()
+            my_password += password
+        
+        elif random_choice == "Space":
+            my_password += " "
+
+        else:
+            pass
+    return my_password
+
+
+
+            
+    
+
+
+        
